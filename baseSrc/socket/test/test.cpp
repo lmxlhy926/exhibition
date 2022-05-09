cbc6122de3777a8372e37c5a011d408a16817dcd
lhy <784723153@qq.com>
2022-05-09 23:28:40 +0800

    出错保存

--- baseSrc/socket/test/test.cpp
+++ baseSrc/socket/test/test.cpp
@@ -1,27 +1,15 @@
 
 #include <iostream>
 #include <unordered_map>
-#include <map>
 #include <thread>
 #include <chrono>
-#include "../objecPtrtHolder.h"
-
-
 #include <string>
-//#include "../socketServer.h"
-//#include "../socketClient.h"
+#include "socket/socketClient.h"
+
 
 
 using namespace std;
 
-#if 0
-void test(){
-    std::unordered_map<string, string> umap;
-    umap.insert(std::make_pair("hello", "first"));
-    umap.insert(std::make_pair("hello", "second"));
-
-    std::cout << umap.find("hello")->second << std::endl;
-}
 
 void clientTest(){
     socketClient sc;
@@ -34,20 +22,23 @@ void clientTest(){
         std::this_thread::sleep_for(std::chrono::seconds(100));
 
 }
-#endif
 
 
-int main(int argc, char* argv[]){
-//    string ip = "127.0.0.1";
-//    int port = 60001;
+void serverTest(){
+    string ip = "127.0.0.1";
+    int port = 60001;
 //    socketServer server;
 //    server.start(ip, 60001);
 //    server.listen();
-//
-//    std::cout << "-----main here-----" << std::endl;
+}
 
-    objectPtrHolder<int> obj;
-    obj.existObject("sss");
+
+int main(int argc, char* argv[]){
+    clientTest();
+
+    std::cout << "-----main here-----" << std::endl;
+
+
 
     while(true)
         std::this_thread::sleep_for(std::chrono::seconds(100));
