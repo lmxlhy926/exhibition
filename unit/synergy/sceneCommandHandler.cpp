dfbe9af42ecb3967e2a412553ea2083e30609c9e
lhy <784723153@qq.com>
2022-05-17 23:52:25 +0800

    处理灯控指令

--- unit/synergy/sceneCommandHandler.cpp
+++ unit/synergy/sceneCommandHandler.cpp
@@ -17,8 +17,38 @@ bool sitePostRequest(const string& ip, int port, qlibc::QData& request, qlibc::Q
 }
 
 
-qlibc::QData construct(const struct controlData& controlCommand, qlibc::QData& data){
+qlibc::QData construct_lightControlCommand(const struct controlData& controlCommand, qlibc::QData& data){
 
+    Json::Value value;
+    value["t"] = "int";
+    if(controlCommand.command == "turnOn"){
+        value["v"] = 1;
+    }else{
+        value["v"] = 0;
+    }
+
+    qlibc::QData command;
+    command.setString("device_id", data.getString("device_id"));
+    command.setString("device_brand", data.getString("device_brand"));
+    command.setString("device_source", data.getString("device_source"));
+    command.setString("device_subid", data.getString("sub_device_id"));
+    command.setString("device_type", data.getString("device_type"));
+    command.setString("stream_id", "devstate");
+    command.setBool("absolute", true);
+    command.setValue("value", value);
+
+    qlibc::QData device_list;
+    device_list.append(command);
+
+    qlibc::QData request;
+    request.putData("device_list", device_list);
+
+    qlibc::QData retData;
+    retData.setString("service_id", "control_device");
+    retData.putData("request", request);
+
+
+    return retData;
 }
 
 
@@ -30,17 +60,18 @@ bool deviceControlHandler(const string& uri, qlibc::QData& message){
     request.setString("service_id", "get_device_list");
     request.setValue("request", Json::nullValue);
 
-    bool ret = sitePostRequest("127.0.0.1", 60003,
+    bool ret = sitePostRequest(ADAPTER_IP, ADAPTER_PORT,
                                              request, response);
+
     if(ret){
         if(response.getInt("code") == 0 && controlCommand.deviceType == "light"){
             qlibc::QData deviceList = response.getData("response").getData("device_list");
             for(int i = 0 ; i < deviceList.size(); i++){
                 qlibc::QData ithData = deviceList.getArrayElement(i);
                 if(ithData.getString("nick_name") == controlCommand.deviceName){
-                    qlibc::QData postData = construct(controlCommand, ithData);
-                    //发送控制请求
-
+                    qlibc::QData commandData = construct_lightControlCommand(controlCommand, ithData);
+                    qlibc::QData controlRet;
+                    sitePostRequest(ADAPTER_IP, ADAPTER_PORT, commandData, controlRet);
                     break;
                 }
             }
--- unit/synergy/sceneCommandHandler.h
+++ unit/synergy/sceneCommandHandler.h
@@ -8,6 +8,10 @@
 
 #include "qlibc/QData.h"
 
+
+static const string ADAPTER_IP = "127.0.0.1";
+static const int ADAPTER_PORT = 60003;
+
 struct controlData {
     const string deviceType;
     const string area;
