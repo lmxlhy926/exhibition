def0c5e0f65140e635925049b0bcb68f16d27081
lmxlhy926 <56338920+lmxlhy926@users.noreply.github.com>
2022-10-14 17:38:28 +0800

    蓝牙站点开关测试

--- unit/bleTest/main.cpp
+++ unit/bleTest/main.cpp
@@ -10,13 +10,29 @@ int main(int argc, char* argv[]){
 
     SiteRecord::getInstance()->addSite(bleSiteName, LocalIp, bleSitePort);
 
-    qlibc::QData open, close, response;
-    open.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\open.json)");
-    close.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\close.json)");
+    qlibc::QData group1, group2, numdata, response;
+    group1.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\group1.json)");
+    group2.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\group2.json)");
+    numdata.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\num.json)");
 
-    for(int i = 0; i < 10; ++i){
-        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, open, response);
-        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, close, response);
+
+    qlibc::QData group1Open = group1.getData("open");
+    qlibc::QData group1Close = group1.getData("close");
+    qlibc::QData group2Open = group2.getData("open");
+    qlibc::QData group2Close = group2.getData("close");
+
+    int num = numdata.getInt("num");
+
+    for(int i = 0; i < num; ++i){
+        std::cout << "i: " << i << std::endl;
+        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group1Open, response);
+        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group2Open, response);
+        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group1Close, response);
+        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group2Close, response);
+    }
+
+    while(true){
+        std::this_thread::sleep_for(chrono::seconds(10));
     }
 
     return 0;
