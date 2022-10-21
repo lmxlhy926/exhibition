121e4f33d86204752994819e3dbd41e099132e3d
78472 <784723153@11.com>
2022-10-21 17:11:55 +0800

    蓝牙测试程序保存

--- unit/bleTest/main.cpp
+++ unit/bleTest/main.cpp
@@ -1,38 +1,40 @@
 
-
 #include <iostream>
 #include "common/httpUtil.h"
 
 int main(int argc, char* argv[]){
     string bleSiteName = "ble_light";
-    string LocalIp = "192.168.58.118";
+    string LocalIp = "192.168.58.116";
     int bleSitePort = 9001;
 
     SiteRecord::getInstance()->addSite(bleSiteName, LocalIp, bleSitePort);
 
-    qlibc::QData group1, group2, numdata, response;
-    group1.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\group1.json)");
-    group2.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\group2.json)");
-    numdata.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\num.json)");
+    qlibc::QData openList, closeList, response;
+    openList.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\open.json)");
+    closeList.loadFromFile(R"(D:\bywg\project\exhibition\unit\bleTest\close.json)");
 
+    for(int index = 0; index < 5; ++index){
+        //依次打开客厅几个组的灯
+        Json::ArrayIndex openListSize = openList.size();
+        for(Json::ArrayIndex i = 0; i < openListSize; ++i){
+            qlibc::QData controlData = openList.getArrayElement(i);
+            SiteRecord::getInstance()->sendRequest2Site(bleSiteName, controlData, response);
+            std::cout << "open: " << i << std::endl;
+        }
 
-    qlibc::QData group1Open = group1.getData("open");
-    qlibc::QData group1Close = group1.getData("close");
-    qlibc::QData group2Open = group2.getData("open");
-    qlibc::QData group2Close = group2.getData("close");
-
-    int num = numdata.getInt("num");
-
-    for(int i = 0; i < num; ++i){
-        std::cout << "i: " << i << std::endl;
-        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group1Open, response);
-        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group2Open, response);
-        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group1Close, response);
-        SiteRecord::getInstance()->sendRequest2Site(bleSiteName, group2Close, response);
+        //依次关闭客厅几个组的灯
+        Json::ArrayIndex closeListSize = closeList.size();
+        for(Json::ArrayIndex i = 0; i < closeListSize; ++i){
+            qlibc::QData controlData = closeList.getArrayElement(i);
+            SiteRecord::getInstance()->sendRequest2Site(bleSiteName, controlData, response);
+            std::cout << "close: " << i << std::endl;
+        }
     }
 
+    std::cout << "-------send complete---------" << std::endl;
+
     while(true){
-        std::this_thread::sleep_for(chrono::seconds(10));
+        std::this_thread::sleep_for(std::chrono::seconds(10));
     }
 
     return 0;
