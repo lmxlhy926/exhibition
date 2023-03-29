232ad5a2664f0a2a19a8e4fb42f2a50ec927de2b
lmxlhy926 <56338920+lmxlhy926@users.noreply.github.com>
2023-03-29 14:14:26 +0800

    白名单消息统一为whiteList

--- unit/config/param.h
+++ unit/config/param.h
@@ -46,7 +46,7 @@ static const string GETAUDIOPANELLIST_REQUEST_SERVICE_ID            =    "getAud
 //消息发布ID
 static const string WHITELIST_MESSAGE_ID =  "whiteList";                           //通知白名单被修改
 static const string RECEIVED_WHITELIST_ID = "receivedWhiteList";                   //通知安装师傅app，收到了白名单
-static const string WHITELIST_MODIFIED_MESSAGE_ID       = "whiteListModified";     //白名单被app修改
+static const string WHITELIST_MODIFIED_MESSAGE_ID       = "whiteList";     //白名单被app修改
 static const string SCENELIST_MODIFIED_MESSAGE_ID       = "sceneListModified";     //场景文件被app修改
 static const string PANELINFO_MODIFIED_MESSAGE_ID       = "self_info_update";      //面板配置信息更改
 static const string RADARDEVICE_RECEIVED_MESSAGE_ID     = "radarDeviceRegister";   //接收雷达设备
