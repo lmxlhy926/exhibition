403942783ddc72b9ff081bddf40790a7acb1a1c5
lhy <784723153@qq.com>
2023-05-12 17:20:01 +0800

    修改shell

--- shell/adbpull.sh
+++ shell/adbpull.sh
@@ -1,14 +1,17 @@
 #! /usr/bin/bash
 
+
+
 for file in $*
 do
     if [ $file == "" ]
     then
-        /mnt/d/adb/adb.exe pull /data/changhong/edge_midware 
+        /mnt/d/adb/adb.exe pull /data/changhong/edge_midware/data $1 
+        /mnt/d/adb/adb.exe pull /data/changhong/edge_midware/panelConfig.txt $1
     elif [ $file == ""]
     then
-
-    elif [ $file == ""]
+        /mnt/d/adb/adb.exe pull /data/changhong/edge_midware/lhy $1
+        
     then
 
     elif [ $file == ""]
--- shell/rkpush.sh
+++ shell/rkpush.sh
@@ -1,12 +1,15 @@
 #! /usr/bin/bash
 
+sourceDir=/home/lhy/smarthome/exhibition/out/arm64
+destDir=/data/changhong/edge_midware
 if [ $1 == "allbin" ]
 then
-     /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/query_site /data/changhong/edge_midware
-     /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/config_site /data/changhong/edge_midware
-     /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/ble_site /data/changhong/edge_midware
-     /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/synergy_site /data/changhong/edge_midware
-    
+    sites=(query_site config_site ble_site synergy_site)
+    for site in ${sites[*]}
+    do
+        /mnt/d/adb/adb.exe push ${sourceDir}/${site} ${destDir}
+        /mnt/d/adb/adb.exe shell chmod 0777 ${destDir}/${site}
+    done
 elif [ $1 == "shell" ]
 then 
     /mnt/d/adb/adb.exe shell
@@ -15,28 +18,24 @@ else
     do
         if [ $site == "query_site" ]
         then
-            /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/query_site /data/changhong/edge_midware
+            /mnt/d/adb/adb.exe push ${sourceDir}/query_site ${destDir}
+            /mnt/d/adb/adb.exe shell chmod 0777 ${destDir}/query_site
         elif [  $site == "config_site" ]
         then
-            /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/config_site /data/changhong/edge_midware
+            /mnt/d/adb/adb.exe push ${sourceDir}/config_site ${destDir}
+            /mnt/d/adb/adb.exe shell chmod 0777 ${destDir}/config_site
         elif [  $site == "ble_site" ]
         then
-            /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/ble_site /data/changhong/edge_midware
+            /mnt/d/adb/adb.exe push ${sourceDir}/ble_site ${destDir}
+            /mnt/d/adb/adb.exe shell chmod 0777 ${destDir}/ble_site
         elif [  $site == "synergy_site" ]
         then
-            /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/synergy_site /data/changhong/edge_midware
-        elif [ $site == "start_edge_midware.sh" ]
-        then
-            /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/start_edge_midware.sh /data/changhong/edge_midware
-        elif [ $site == "wpa_supplicant.conf" ]
-        then
-            /mnt/d/adb/adb.exe push /home/lhy/smarthome/exhibition/out/arm64/wpa_supplicant.conf /data/changhong/edge_midware
+            /mnt/d/adb/adb.exe push ${sourceDir}/synergy_site ${destDir}
+            /mnt/d/adb/adb.exe shell chmod 0777 ${destDir}/synergy_site
         fi
     done
 fi
 
 echo "---------------------------------------------------------------------------------"
 /mnt/d/adb/adb.exe shell ls -lht /data/changhong/edge_midware/query_site /data/changhong/edge_midware/config_site /data/changhong/edge_midware/ble_site /data/changhong/edge_midware/synergy_site
-# echo "---------------------------------------------------------------------------------"
-# /mnt/d/adb/adb.exe shell ps -ef|grep site
-# echo "---------------------------------------------------------------------------------"
+
